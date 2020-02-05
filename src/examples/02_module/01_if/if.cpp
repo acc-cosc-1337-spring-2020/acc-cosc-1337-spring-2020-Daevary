a5c9fca91ac166f0921dd634d947fbe646964769
Daevary <daevary.ramirez@g.austincc.edu>
2020-02-04 18:40:17 -0600

    boolean expressions, if, cin

--- src/examples/02_module/01_if/if.cpp
+++ src/examples/02_module/01_if/if.cpp
@@ -3,7 +3,11 @@
 
 
 //write code for the is_even function that returns true if num is even, false otherwise
+bool is_even(int num) 
+{
+	return num %2 == 0; // any number divided by 2 equals 0
 
+}
 
 //write code for function named overtime that accepts a double hours and returns true if 
 //hours over 40, false otherwise
