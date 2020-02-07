fff4e395939036ea88120b8b695036989bab078a
Daevary <daevary.ramirez@g.austincc.edu>
2020-02-06 20:05:43 -0600

    switch cases and assignment 2: calculating gross salary including overtime hours

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,13 @@
+#include "decision.h"
 //Write the function code that returns the product of hours and hourly_rate.
+double gross_pay(double hours, double hourly_payrate)
+{
+	if (hours > 40)
+	{
+		double oT = hours - 40;
+		return((hours - oT) * hourly_payrate) + (hourly_payrate * 1.5 * oT);
+	}
+	
+	return hours * hourly_payrate;
+}
+	
--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -1,23 +1,36 @@
 //Write the include statement for decisions.h here
+#include "decision.h"
 
 //Write include for capturing input from keyboard and displaying output to screen
+#include<iostream>
 
 
 //Write namespace using statements for cout and cin
+using std::cout; using std::cin;
 
 int main() 
 {
 	//create a double variable named hours
 	//create a double variable named hourly_rate
+	double hours, hourly_payrate;
 
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
+	cout << "Enter hours worked: ";
+	cin >> hours;
+
+	cout << "Enter your hourly pay rate: ";
+	cin >> hourly_payrate;
 
 	//Create a double variable named gross_salary
 	//Call the gross_pay function and save its return value to the gross_salary variable
+	double gross_salary;
+	gross_salary = gross_pay(hours, hourly_payrate);
+
 
 	//Display the gross_salary variable as follows:
 	//Example ---->>>     Gross pay: 150.00   
+	cout << "Gross Pay: " << gross_salary;
 
 	return 0;
 }
