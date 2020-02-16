57741ca8377dad5e2b75992cdae2f722bef709b9
daevary <daevary.ramirez@g.austincc.edu>
2020-02-15 20:12:20 -0600

    homework 2 corrections grades

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -8,11 +8,11 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Test get grade points")
 {
-	REQUIRE(get_grade_points(A) == 4);
-	REQUIRE(get_grade_points(B) == 3);
-	REQUIRE(get_grade_points(C) == 2);
-	REQUIRE(get_grade_points(D) == 1);
-	REQUIRE(get_grade_points(F) == 0);
+	REQUIRE(get_grade_points("A") == 4);
+	REQUIRE(get_grade_points("B") == 3);
+	REQUIRE(get_grade_points("C") == 2);
+	REQUIRE(get_grade_points("D") == 1);
+	REQUIRE(get_grade_points("F") == 0);
 
 }
 
