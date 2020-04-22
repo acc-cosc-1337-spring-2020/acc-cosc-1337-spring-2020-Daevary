1e133126bcf13837afadc4a56b278f74d04b5e61
daevary <Daevary.ramirez@g.austincc.edu>
2020-04-21 18:13:19 -0600

    class cop stuff

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,2 +1,25 @@
 //
 //TODO 7 add template
+
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+#include <cstddef> // size_t datatype
+#include <stddef.h>
+
+class Vector
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v); //copy constructor
+	size_t Size()const { return size; }
+	int& operator[](int i) { return nums[i]; } 
+	int& operator[](int i)const { return nums[i]; }
+
+private:
+	size_t size; //stack variable
+	int*nums; // telling compiler to save memory 
+
+
+};
+
+#endif
